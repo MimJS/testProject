6c54489a4dd3bcfbd955878139b642aeb2575d06
MimJS <88341555+MimJS@users.noreply.github.com>
2022-07-09 01:15:52 +0300

    Delete src/api directory

--- src/api/getEvents/getEvents.js
+++
@@ -1,8 +0,0 @@
-const { dbReq } = require("../../lib/modules/mysql");
-
-module.exports = async () => {
-  const value = await dbReq(
-    `SELECT id, author, photo, name, date, description, createdDate, eventType, projectBase, source, dateEnd, author_login, timeStart, hasMinted FROM events WHERE dateEnd >= ${Date.now()} AND isModer = 1 ORDER BY date ASC`
-  );
-  return value;
-};
