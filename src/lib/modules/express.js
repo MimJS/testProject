30c1b7e3fbe6f38e8c5f050e7d655cf3faeda23b
MimJS <88341555+MimJS@users.noreply.github.com>
2022-07-15 00:48:05 +0300

    Add files via upload

--- src/lib/modules/express.js
+++ src/lib/modules/express.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const resolveRequest = require("../../api");
 const { port, postUrl } = require("../cfgs/config.express");
-const cors = require('cors');
+const https = require("https");
+const fs = require("fs");
+const cors = require("cors");
 
-const createServer = (callback) => {
+const createServer = (callback, ssl) => {
   const app = express();
   app.use(express.json());
-  app.use(cors({
-      origin: '*'
-  }));
+  app.use(
+    cors({
+      origin: "*",
+    })
+  );
   app.post(postUrl, (req, res) => {
     console.log("New request");
     resolveRequest(req, res);
@@ -16,8 +20,16 @@ const createServer = (callback) => {
   app.get("/", (req, res) => {
     res.sendStatus(503);
   });
-  app.listen(port, () => console.log(`port listen: ${port}`));
-  callback();
+  if (ssl) {
+    const options = {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    };
+    https.createServer(options, app).listen(443);
+  } else {
+    app.listen(port, () => console.log(`port listen: ${port}`));
+    callback();
+  }
 };
 
 module.exports = { createServer };
